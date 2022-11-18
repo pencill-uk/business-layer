5422425452c075f3ec59b93a96eada915ed802fe
pencill-ram <ram@pencill.co.uk>
2022-11-18 17:57:19 +0000

    User Entity

---
+++ lib/user_entity.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library business_layer;
+
+export 'src/user/authenticate/bloc/authenticate_bloc.dart';
+export 'src/model/user.dart';
+// TODO: Export any libraries intended for clients of this package.
